1b42259e8e851dc4fc9dbedacdd3b9d84db0b31b
fienestar <fienestar@gmail.com>
2023-07-20 02:46:54 +0900

    feat: add test and eslint

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+};
+
+export default config;
