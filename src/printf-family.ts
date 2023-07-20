09783f57af524210090ca13da8b697cd4391ae63
fienestar <fienestar@gmail.com>
2023-07-20 15:29:16 +0900

    fix: vprintf and printf not returning length

--- src/printf-family.ts
+++ src/printf-family.ts
@@ -2,17 +2,17 @@
 import sprintf from './sprintf';
 import {FormatArgument} from './types';
 
-export function vsprintf<Format extends string>(format: Format, args: FormatArgument<Format>) {
+export function vsprintf<Format extends string>(format: Format, args: FormatArgument<Format>): string {
     return sprintf(format, ...args);
 }
 
-export function vprintf<Format extends string>(format: Format, args: FormatArgument<Format>) {
+export function vprintf<Format extends string>(format: Format, args: FormatArgument<Format>): number {
     const text = vsprintf(format, args);
     console.log(text);
-    return text;
+    return text.length;
 }
 
-export function printf<Format extends string>(format: Format, ...args: FormatArgument<Format>) {
+export function printf<Format extends string>(format: Format, ...args: FormatArgument<Format>): number {
     return vprintf(format, args);
 }
 
